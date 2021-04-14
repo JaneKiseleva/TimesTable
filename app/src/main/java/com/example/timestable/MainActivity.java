f593fbdcb0d87be4a9ae176f57d2bac0a2ce53a9
JaneKiseleva <pro.kiseleva@gmail.com>
2021-04-14 05:00:00 +0300

    version 3

--- app/src/main/java/com/example/timestable/MainActivity.java
+++ app/src/main/java/com/example/timestable/MainActivity.java
@@ -25,23 +25,19 @@ public class MainActivity extends AppCompatActivity {
         seekBar.setMax(max);
         listViewNumbers = findViewById(R.id.listViewNumbers);
         numbers = new ArrayList<>();
-        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_list_item_1, numbers);
+        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, numbers);
         listViewNumbers.setAdapter(arrayAdapter);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                numbers.clear(); //очищаем список и теперь цифры добавляются в зависимости от прогресса не вниз по экрану, а заменяют старые
-                //проверка про ноль
-                if(progress <min) {
+                numbers.clear();
+                if (progress < min) {
                     seekBar.setProgress(min);
                     numbers.clear();
                 }
                 for (int i = min; i <= count; i++) {
-                    //убираем нули в списке, поэтому заменяем переменую progress на seekBar.getProgress() и в самом сигбаре на эмуляторе ы упираемся в единицу
                     numbers.add(seekBar.getProgress() * i);
                 }
-                //Адаптер на этом моменте еoе не знает что у нас какие-то цифры в массиве есть, надо ему сообщить. Есть для этого метод notifyDataSetChanged
-                //Говорим адаптеру, обрати внимание, что данные изменились.
                 arrayAdapter.notifyDataSetChanged();
             }
 
@@ -53,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
-        //Теперь хотим, чтобы числа добавлялись при запуске нашей программы, еще до того, как пользователь начнет двигать кружок на SeekBar (и начинает работать метод onProgressChanged
         seekBar.setProgress(10);
     }
 }
